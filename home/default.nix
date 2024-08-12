be78aa19fc412a4a0c001b47a559598fb4631574
Patrick Materer <patrickmaterer@gmail.com>
2024-08-12 15:48:09 -0500

    Split up files

--- home/default.nix
+++ home/default.nix
@@ -1,5 +1,5 @@
 { pkgs, config, nixvim, agenix, defaultEmail, ... }: {
-  imports = [ nixvim.homeManagerModules.nixvim ];
+  imports = [ ./packages.nix nixvim.homeManagerModules.nixvim ];
 
   catppuccin = {
     enable = false;
@@ -21,108 +21,8 @@
     file.".npmrc".text = ''
       prefix=~/.npm
     '';
-
-    packages = with pkgs;
-      [
-        # admin tools
-        coreutils
-        neofetch
-        colordiff
-        ripgrep
-        tree
-        yq
-        gnutar
-        gnumake
-        unzip
-
-        dnsmasq
-
-        ffmpeg
-
-        agenix.packages.${system}.default
-
-        # etc
-        imagemagick
-        nerdfonts
-        cowsay
-        fortune
-        lolcat
-        gmailctl
-        graphviz
-        element
-
-        # fonts
-        monaspace
-        fantasque-sans-mono
-
-        # git
-        ghorg
-        pre-commit
-        glab
-
-        # development
-        shellcheck
-        python3
-        nodejs_latest
-        yarn
-        asdf-vm
-
-        # networking
-        ipcalc
-        curl
-
-        #nix
-        nixfmt-classic
-        nil # nix lsp
-
-        # devops
-        tflint
-        terragrunt
-        terrascan
-
-        # vms
-        packer
-
-        # cloud
-        aws-sso-cli
-        awscli
-
-        # k8s
-        kubectl
-        kubernetes-helm
-        kubectx
-        argo
-        argocd
-        eks-node-viewer
-
-        # charmbracelet
-        glow
-        vhs
-        pop
-        gum
-        skate
-      ] ++ (if pkgs.stdenv.isLinux then
-        with pkgs; [
-          terraform-docs
-          vagrant
-          spotify-player
-          iw
-          qemu
-          OVMF
-          libvirt
-          virt-viewer
-          virt-manager
-          pciutils
-          glxinfo
-          lshw
-
-          libxslt # for libvirt Terraform
-
-          xclip
-        ]
-      else
-        [ ]);
   };
+
   xdg = {
     enable = true;
 
---
+++ home/packages.nix
@@ -0,0 +1,103 @@
+{ pkgs, agenix, ... }: {
+  home.packages = with pkgs;
+    [
+      # admin tools
+      coreutils
+      neofetch
+      colordiff
+      ripgrep
+      tree
+      yq
+      gnutar
+      gnumake
+      unzip
+
+      dnsmasq
+      step-ca
+
+      ffmpeg
+
+      agenix.packages.${system}.default
+
+      # etc
+      imagemagick
+      nerdfonts
+      cowsay
+      fortune
+      lolcat
+      gmailctl
+      graphviz
+      element
+
+      # fonts
+      monaspace
+      fantasque-sans-mono
+
+      # git
+      ghorg
+      pre-commit
+      glab
+
+      # development
+      shellcheck
+      python3
+      nodejs_latest
+      yarn
+      asdf-vm
+
+      # networking
+      ipcalc
+      curl
+
+      #nix
+      nixfmt-classic
+      nil # nix lsp
+
+      # devops
+      tflint
+      terragrunt
+      terrascan
+
+      # vms
+      packer
+
+      # cloud
+      aws-sso-cli
+      awscli
+
+      # k8s
+      kubectl
+      kubernetes-helm
+      kubectx
+      argo
+      argocd
+      eks-node-viewer
+
+      # charmbracelet
+      glow
+      vhs
+      pop
+      gum
+      skate
+    ] ++ (if pkgs.stdenv.isLinux then
+      with pkgs; [
+        terraform-docs
+        vagrant
+        spotify-player
+        iw
+        qemu
+        OVMF
+        libvirt
+        virt-viewer
+        virt-manager
+        pciutils
+        glxinfo
+        lshw
+
+        libxslt # for libvirt Terraform
+
+        xclip
+      ]
+    else
+      [ ]);
+}
