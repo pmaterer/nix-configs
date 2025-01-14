3a6cedc743a9d234c8089a456c603f5af5aa5821
Patrick Materer <patrickmaterer@gmail.com>
2025-01-14 12:27:11 -0600

    Updates

--- home/zsh/default.nix
+++ home/zsh/default.nix
@@ -27,6 +27,7 @@ in {
     export PATH="$HOME/.krew/bin:$PATH"
     export PATH="$HOME/bin:$PATH"
     export PATH="$HOME/.local/bin.go:$PATH"
+    export PATH="$HOME/nonix-bin:$PATH"
 
     export PYENV_ROOT="$HOME/.pyenv"
     [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
